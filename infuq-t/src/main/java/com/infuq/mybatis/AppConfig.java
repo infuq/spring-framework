695236a72ddcb8a8afcb1d470610cd8a21463bce
infuq <infuq217@outlook.com>
2021-12-25 13:32:02 +0800

    ...

--- infuq-t/src/main/java/com/infuq/mybatis/AppConfig.java
+++ infuq-t/src/main/java/com/infuq/mybatis/AppConfig.java
@@ -45,10 +45,10 @@ public class AppConfig {
 
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource druidDataSource) throws Exception {
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(druidDataSource);
+        factoryBean.setDataSource(dataSource);
         return factoryBean.getObject();
 
     }
